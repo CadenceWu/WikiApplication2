5709a13074a05ba27c5ec385db6007b82dbcb536
CadenceWu <90173356+CadenceWu@users.noreply.github.com>
2022-05-23 20:39:09 +0800

    23/05/2022

--- WikiApplication2/Wiki Application2.cs
+++ WikiApplication2/Wiki Application2.cs
@@ -31,7 +31,7 @@ namespace WikiApplication2
         private void displayInformation()
         {
             listViewWiki.Items.Clear();
-
+            sortData();
             foreach (information info in Wiki)
             {
                 ListViewItem item;
@@ -41,9 +41,12 @@ namespace WikiApplication2
                 item.SubItems.Add(info.getDefinition());
             }
         }
-        private void vlaidName()
+        private bool validName(string checkTheName)
         {
-          
+            if (Wiki.Exists(x=> x.getName().Equals(checkTheName)))
+                return false;
+            else
+                return true;
         }
         private Boolean highLightValue()
         {
@@ -72,35 +75,138 @@ namespace WikiApplication2
             {
                 selectRadio = radioButtonNonLinear.Text;
             }
-            MessageBox.Show(selectRadio);
+           // MessageBox.Show(selectRadio);
             return selectRadio;
+        }
+        private void sortData() {
+
+           Wiki = Wiki.OrderBy(o => o.getName()).ToList();
+           //listViewWiki.Sorting = SortOrder.Ascending;
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-            information newInfo = new information();
-            newInfo.setName(textBoxName.Text);
-            newInfo.setCategory(comboBoxCategory.Text.ToString());
-            newInfo.setStructure(returnValue(selectRadio));
-            newInfo.setDefinition(textBoxDefinition.Text);
-            Wiki.Add(newInfo);
+
+            if (!validName(textBoxName.Text))
+            {
+                MessageBox.Show("The name is already existed");
+            }
+
+            else if (string.IsNullOrWhiteSpace(textBoxName.Text) && string.IsNullOrWhiteSpace(comboBoxCategory.Text) &&
+                string.IsNullOrWhiteSpace(returnValue(selectRadio)) && string.IsNullOrWhiteSpace(textBoxDefinition.Text))
+            {
+                MessageBox.Show("Some text boxes are empty.");
+            }
+            else 
+            {
+                information newInfo = new information();
+                newInfo.setName(textBoxName.Text);
+                newInfo.setCategory(comboBoxCategory.Text.ToString());
+                newInfo.setStructure(returnValue(selectRadio));
+                newInfo.setDefinition(textBoxDefinition.Text);
+                Wiki.Add(newInfo);
+            }
             clearTextbox();
             displayInformation();
         }
 
         private void buttonDelete_Click(object sender, EventArgs e)
         {
+            listViewWiki.Items.RemoveAt(listViewWiki.SelectedIndices[0]);
+
+           // listViewWiki.SelectedItems[0].Remove();
+            //Wiki.Remove();
+
+            //foreach (information info in Wiki)
+            //{
+                
+            //    //if(info.getName().Equals(textBoxName.Text))
+            //    //{
+
+            //    //}
+            //    info.setName(textBoxName.Text);
+            //    info.setCategory(comboBoxCategory.Text.ToString());
+            //    info.setStructure(returnValue(selectRadio));
+            //    info.setDefinition(textBoxDefinition.Text);
+            //    Wiki.Remove(info);
+            //}
+            //displayInformation();
+            // Wiki.RemoveAt(listViewWiki.Items.IndexOf(itemSelected.Index));
+
+            //foreach (ListViewItem itemSelected in listViewWiki.SelectedItems)
+
+            //{
+            //    listViewWiki.Items.Remove(itemSelected);
+            //    //listViewWiki.SelectedIndices.Remove(itemSelected.Index);
+
+            //    //information newInfo = new information();
+            //    //newInfo.setName(textBoxName.Text);
+            //    //newInfo.setCategory(comboBoxCategory.Text.ToString());
+            //    //newInfo.setStructure(returnValue(selectRadio));
+            //    //newInfo.setDefinition(textBoxDefinition.Text);
+            //    //Wiki.Remove(newInfo);
+
+            //   
+
+            //    //if (itemSelected != null)
+            //    //{
+
+
+            //    //foreach (information item in Wiki) 
+            //    //{
+
+            //    //}
+            //    //}
+
+            //}
+            //// var itemRemove = Wiki.Single(r => r.getName() == listViewWiki.SelectedIndicese[0].SubItems[0].Text);
+            // Wiki.Remove(itemRemove);
+            // displayInformation();
 
         }
 
         private void buttonEdit_Click(object sender, EventArgs e)
         {
 
+            if (!validName(textBoxName.Text))
+            {
+                MessageBox.Show("The name is already existed");
+            }
+            else if (string.IsNullOrWhiteSpace(textBoxName.Text) && string.IsNullOrWhiteSpace(comboBoxCategory.Text) &&
+                string.IsNullOrWhiteSpace(returnValue(selectRadio)) && string.IsNullOrWhiteSpace(textBoxDefinition.Text))
+            {
+                MessageBox.Show("Some text boxes are empty.");
+            }
+            else 
+            {
+                listViewWiki.SelectedItems[0].Text = textBoxName.Text;
+                listViewWiki.SelectedItems[0].SubItems[1].Text = comboBoxCategory.Text;
+                listViewWiki.SelectedItems[0].SubItems[3].Text = textBoxDefinition.Text;
+                listViewWiki.SelectedItems[0].SubItems[2].Text = returnValue(selectRadio);
+                clearTextbox();
+                sortData();
+            }
         }
 
         private void buttonSearch_Click(object sender, EventArgs e)
         {
-
+            //Wiki.BinarySearch();
+            //if (!string.IsNullOrWhiteSpace(textBoxName.Text))
+            //{
+            //    information searchInfo=new information();
+            //    textBoxSearch.Text=searchInfo.getName();
+            //    sortData();
+            //    if (Wiki.BinarySearch(searchInfo.getName()) >=0)
+            //        MessageBox.Show("Found");
+            //    else
+            //    { }
+            //        MessageBox.Show("Not found");
+            //        textBoxSearch.Clear();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Please enter a plate");
+            //}
         }
 
         private void buttonOpen_Click(object sender, EventArgs e)
@@ -138,7 +244,7 @@ namespace WikiApplication2
 
         private void listViewWiki_MouseClick(object sender, MouseEventArgs e)
         {
-            String name=listViewWiki.SelectedItems[0].SubItems[0].Text;
+            String name = listViewWiki.SelectedItems[0].SubItems[0].Text;
             String category = listViewWiki.SelectedItems[0].SubItems[1].Text;
             String definition = listViewWiki.SelectedItems[0].SubItems[3].Text;
             highLightValue();
@@ -146,7 +252,7 @@ namespace WikiApplication2
             comboBoxCategory.Text = category;
             textBoxDefinition.Text = definition;
 
-
         }
+
     }
 }
